106268005f06d27d0593fd55f178b74856fc8c3a
GabrielaGonzaga <gabrielamgonzaga1@gmail.com>
2021-05-05 17:58:52 -0300

    SPMEG p/1

---
+++ Sprint2-Senai-2021/Senai.SPMEDG.webApi/senai.SPMEG.webApi/senai.SPMEG.webApi/Startup.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.SPMEG.webApi
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services
+               //Forma de autenticação
+               .AddAuthentication(options =>
+               {
+                   options.DefaultAuthenticateScheme = "JwtBearer";
+                   options.DefaultChallengeScheme = "JwtBearer";
+               })
+
+               //Definição dos parâmetros
+               .AddJwtBearer("JwtBearer", options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                        //quem esta solicitando a ação
+                        ValidateIssuer = true,
+
+                        //quem esta recebendo a ação
+                        ValidateAudience = true,
+
+                        //tempo de expiração
+                        ValidateLifetime = true,
+
+                       IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("Semprepea10_perfis")),
+
+                        //tempo de expiração
+                        ClockSkew = TimeSpan.FromMinutes(45),
+
+                        //Nome do issuer, de onde está vindo 
+                        ValidIssuer = "SPMEDG.webApi",
+
+                        //nome do audience, para aonde vai
+                        ValidAudience = "SPMEDG.webApi"
+                   };
+               });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // Define o mapeamento dos Controllers
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
